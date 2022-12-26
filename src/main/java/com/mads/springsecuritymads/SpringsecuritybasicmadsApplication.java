491177986f7776e9532d104ecc9bf970daf79ac1
Santiago Sáez Caselles <ssc64@mscloud.ua.es>
2022-12-26 10:22:08 +0100

    First commit

---
+++ src/main/java/com/mads/springsecuritymads/SpringsecuritybasicmadsApplication.java
@@ -0,0 +1,13 @@
+package com.mads.springsecuritymads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicmadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicmadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mads/springsecuritymads/SpringsecuritybasicmadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mads.springsecuritymads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicmadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
